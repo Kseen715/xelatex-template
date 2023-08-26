17e30adfaa9669f2851d0afdc189d44dbd7efb7f
kseen <korenevdn@gmail.com>
2023-08-27 02:59:21 +0300

    Setup scripts update

--- tex-setup.ps1
+++ tex-setup.ps1
@@ -2,4 +2,5 @@ Remove-Item -Path .\.git -Recurse -Force
 Remove-Item -Path .\README.md -Force
 Remove-Item -Path .\.gitignore -Force
 Remove-Item -Path .\LICENSE -Force
+Remove-Item -Path .\tex-setup.sh -Force
 Remove-Item -Path .\tex-setup.ps1 -Force
--- tex-setup.sh
+++ tex-setup.sh
@@ -4,4 +4,5 @@ rm -rf .git
 rm README.md
 rm .gitignore
 rm LICENSE
+rm tex-setup.ps1
 rm tex-setup.sh
